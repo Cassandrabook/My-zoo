4feee6f3689b66043d6fd2ca5b282fb6f32ec363
Your Namecl <you@example.com>
2023-03-29 11:16:40 +0200

    added error image function

--- src/components/Animal/Animal.tsx
+++ src/components/Animal/Animal.tsx
@@ -1,53 +1,73 @@
 import { useNavigate } from "react-router-dom";
 import { IAnimal } from "../../models/IAnimal";
 import { IAnimalSmall } from "../../models/IAnimalSmall";
-import './Animal.scss';
+import errorImage from "./../../assets/image_not_available1.94c0c57d.png";
+import "./Animal.scss";
 
-interface IAnimalProps{
-    animal: IAnimalSmall;
+interface IAnimalProps {
+  animal: IAnimalSmall;
 }
 
-export const Animal = (props: IAnimalProps)=> {
-    const navigate = useNavigate();
+export function imageError(e: any) {
+  e.currentTarget.src = errorImage;
+}
 
-    const showAnimal = () => {
-        navigate(`/animal/${props.animal.id}`);
-    };
-    
-    function isFeed(){
-        const animals = localStorage.getItem("Animals");
-        if(animals){
-            const array = JSON.parse(animals);
-            const animal = array.find((animal:IAnimal) => animal.id === Number(props.animal.id)) || null;
-            if(animal){
-                const lastFed = new Date(animal.lastFed);
-                const rightNow = new Date();
+export const Animal = (props: IAnimalProps) => {
+  const navigate = useNavigate();
 
-                const timeDiffInMs = rightNow.getTime() - lastFed.getTime();
-                const diffInSeconds = timeDiffInMs / 1000; 
-                const diffInMinutes = diffInSeconds / 60;
-                const diffInHours = diffInMinutes / 60;
+  const showAnimal = () => {
+    navigate(`/animal/${props.animal.id}`);
+  };
 
-                if(diffInHours > 3){
-                    return false;
-                }else{
-                    return true;
-                }
-            }
-        } 
+  function isFeed() {
+    const animals = localStorage.getItem("Animals");
+    if (animals) {
+      const array = JSON.parse(animals);
+      const animal =
+        array.find(
+          (animal: IAnimal) => animal.id === Number(props.animal.id)
+        ) || null;
+      if (animal) {
+        const lastFed = new Date(animal.lastFed);
+        const rightNow = new Date();
+
+        const timeDiffInMs = rightNow.getTime() - lastFed.getTime();
+        const diffInSeconds = timeDiffInMs / 1000;
+        const diffInMinutes = diffInSeconds / 60;
+        const diffInHours = diffInMinutes / 60;
+
+        if (diffInHours > 3) {
+          return false;
+        } else {
+          return true;
+        }
+      }
     }
+  }
 
-    return(
-        <>
-            <div className="animal">
-                <h4 className="animal__title">{props.animal.name}</h4>
-                <div className="animal__image-container">
-                    <img src={props.animal.imageUrl} alt={props.animal.name} />
-                </div>
-                <p className="animal__desc">{props.animal.shortDescription}</p>
-                <p className="animal__foodTime">{isFeed() ? <span>{props.animal.name} är mätt</span> : <span className="isHungry">{props.animal.name} är hungrig</span>}</p>
-                <button className="animal__btn-readMore" onClick={showAnimal}>Läs mer</button>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="animal">
+        <h4 className="animal__title">{props.animal.name}</h4>
+        <div className="animal__image-container">
+          <img
+            src={props.animal.imageUrl}
+            alt={props.animal.name}
+            onError={imageError}
+          />
+        </div>
+        <p className="animal__desc">{props.animal.shortDescription}</p>
+        <p className="animal__foodTime">
+          {isFeed() ? (
+            <span>{props.animal.name} är mätt</span>
+          ) : (
+            <span className="isHungry">{props.animal.name} är hungrig</span>
+          )}
+        </p>
+        <button className="animal__btn-readMore" onClick={showAnimal}>
+          Läs mer
+        </button>
+      </div>
+    </>
+  );
+};
--- src/components/AnimalDetail/AnimalDetail.tsx
+++ src/components/AnimalDetail/AnimalDetail.tsx
@@ -1,100 +1,114 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { IAnimal } from '../../models/IAnimal';
-import { getAnimals } from '../../services/AnimalService';
-import './AnimalDetail.scss';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IAnimal } from "../../models/IAnimal";
+import { getAnimals } from "../../services/AnimalService";
+import { imageError } from "../Animal/Animal";
+import "./AnimalDetail.scss";
 
 export const AnimalDetail = () => {
-    const {id} = useParams();
-    const [animals, setAnimals] = useState<IAnimal[]>([]); 
-    const [buttondisabled, setButtondisabled] = useState(false);
-    const [fedTime, setFedTime] = useState("");
-    const [pet, setPet] = useState<IAnimal | null>(null);
-    
-    useEffect( () => {
-      const getData = async () => {
-        
-        if (id) {
-            let response = await getAnimals();
+  const { id } = useParams();
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+  const [buttondisabled, setButtondisabled] = useState(false);
+  const [fedTime, setFedTime] = useState("");
+  const [pet, setPet] = useState<IAnimal | null>(null);
 
-            if(response) {
-                setAnimals(response as any);
-            }
+  useEffect(() => {
+    const getData = async () => {
+      if (id) {
+        let response = await getAnimals();
+
+        if (response) {
+          setAnimals(response as any);
         }
-    };
-    
-      const animalsFromLocalStorage = localStorage.getItem("Animals");
-      if(animalsFromLocalStorage){
-        setAnimals(JSON.parse(animalsFromLocalStorage));
-      }else{
-        getData();
       }
-    }, []);
+    };
 
-    useEffect(() => {
-      const animal = animals.find((animal:IAnimal) => animal.id === Number(id)) || null;
-      
-        if(animal){
-          setFedTime(animal.lastFed);
-          setPet(animal);
-
-          const lastFed = new Date(animal.lastFed);
-          const rightNow = new Date();
-
-          const timeDiffInMs = rightNow.getTime() - lastFed.getTime();
-          const diffInSeconds = timeDiffInMs / 1000; 
-          const diffInMinutes = diffInSeconds / 60;
-          const diffInHours = diffInMinutes / 60;
-
-          if(diffInHours > 3){
-            setButtondisabled(false);
-          }else{
-            setButtondisabled(true);
-          }
-        }   
-    }, [animals]);
-
-      const handleFeedClick = () => {
-        const timeElapsed = Date.now();
-        const rightNow = new Date(timeElapsed);
-        const rightNowAsString = rightNow.toString();
-        const updatedTime = animals.map((animal) => {
-          if (animal.id === pet?.id) {
-            return {
-              ...animal,
-              isFed: true,
-              lastFed: rightNowAsString,
-            };
-          }
-          return animal;
-        });
-      
-        localStorage.setItem("Animals", JSON.stringify(updatedTime));
-        setAnimals(updatedTime);  
-        setButtondisabled(true); 
-        setFedTime(rightNowAsString);
-      };
-   
-    return(
-            <>    
-              <div className='animalDetail'>
-                  <div className='animalDetail__image'>
-                      <img src={pet?.imageUrl} alt={pet?.name} />
-                  </div>
-                  <div className='animalDetail__text-container'>
-                      <h2 className='animalDetail__name'>{pet?.name}</h2>
-                      <p className='animalDetail__desc'>{pet?.longDescription}</p>
-                      <p>{pet?.name} kan matas igen efter 3 timmar.</p>
-                      <p className='animalDetail__birthday'><span>Födelseår: </span>{pet?.yearOfBirth}</p>
-                      <p className='animalDetail__medicine'><span>Mediciner: </span>{pet?.medicine}</p>
-                      <p><span>Matad senast: </span>{fedTime}</p>
-                      <button className='animalDetail__btn' 
-                        disabled={buttondisabled} 
-                        onClick={handleFeedClick}>
-                          {buttondisabled === true ? "Kom tillbaka om 3 timmar" : "Mata djuret"}</button>
-                      <p></p>
-                  </div>
-              </div> 
-            </>
-        );
+    const animalsFromLocalStorage = localStorage.getItem("Animals");
+    if (animalsFromLocalStorage) {
+      setAnimals(JSON.parse(animalsFromLocalStorage));
+    } else {
+      getData();
     }
+  }, []);
+
+  useEffect(() => {
+    const animal =
+      animals.find((animal: IAnimal) => animal.id === Number(id)) || null;
+
+    if (animal) {
+      setFedTime(animal.lastFed);
+      setPet(animal);
+
+      const lastFed = new Date(animal.lastFed);
+      const rightNow = new Date();
+
+      const timeDiffInMs = rightNow.getTime() - lastFed.getTime();
+      const diffInSeconds = timeDiffInMs / 1000;
+      const diffInMinutes = diffInSeconds / 60;
+      const diffInHours = diffInMinutes / 60;
+
+      if (diffInHours > 3) {
+        setButtondisabled(false);
+      } else {
+        setButtondisabled(true);
+      }
+    }
+  }, [animals]);
+
+  const handleFeedClick = () => {
+    const timeElapsed = Date.now();
+    const rightNow = new Date(timeElapsed);
+    const rightNowAsString = rightNow.toString();
+    const updatedTime = animals.map((animal) => {
+      if (animal.id === pet?.id) {
+        return {
+          ...animal,
+          isFed: true,
+          lastFed: rightNowAsString,
+        };
+      }
+      return animal;
+    });
+
+    localStorage.setItem("Animals", JSON.stringify(updatedTime));
+    setAnimals(updatedTime);
+    setButtondisabled(true);
+    setFedTime(rightNowAsString);
+  };
+
+  return (
+    <>
+      <div className="animalDetail">
+        <div className="animalDetail__image">
+          <img src={pet?.imageUrl} alt={pet?.name} onError={imageError} />
+        </div>
+        <div className="animalDetail__text-container">
+          <h2 className="animalDetail__name">{pet?.name}</h2>
+          <p className="animalDetail__desc">{pet?.longDescription}</p>
+          <p className="animalDetail__birthday">
+            <span>Födelseår: </span>
+            {pet?.yearOfBirth}
+          </p>
+          <p className="animalDetail__medicine">
+            <span>Mediciner: </span>
+            {pet?.medicine}
+          </p>
+          <p>
+            <span>Matad senast: </span>
+            {fedTime}
+          </p>
+          <button
+            className="animalDetail__btn"
+            disabled={buttondisabled}
+            onClick={handleFeedClick}
+          >
+            {buttondisabled === true
+              ? "Kom tillbaka om 3 timmar"
+              : `Mata ${pet?.name}`}
+          </button>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+};
